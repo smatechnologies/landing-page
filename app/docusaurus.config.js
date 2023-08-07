9f3685e404eabaf63caafa9f83c511bd1ee99772
Ryan Dimick <rdimick@smatechnologies.com>
2023-08-07 15:50:58 -0500

    Add OpCon core versioning

--- app/docusaurus.config.js
+++ app/docusaurus.config.js
@@ -107,7 +107,22 @@ module.exports = {
         sidebarPath: require.resolve('../content/smatechnologies/opcon-docs/sidebars.js'),
         editUrl:
             'https://github.com/smatechnologies/opcon-docs/blob/main/',
-
+            lastVersion: "current",
+        versions: {
+          current: {
+            label: "current",
+          },
+          "22.0": {
+            label: "22.0",
+            path: "v22.0",
+            banner: "none",
+          },
+          "21.0": {
+            label: "21.0",
+            path: "v21.0",
+            banner: "none",
+          },
+        },    
       },
     ],
     [
