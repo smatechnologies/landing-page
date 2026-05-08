f5ecfc085835da289f6eb84e2583a5db07606bcf
Roy Weesner <rweesner@smatechnologies.com>
2026-05-08 09:27:18 -0500

    Revert "Add lesson-style component proof of concept"
    This reverts commit 554808ed0533c082acf5463c695c468cf80b5063.

--- app/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as LessonHeader } from './LessonHeader';
-export { default as Section } from './Section';
-export { default as WhyItMatters } from './WhyItMatters';
-export { Takeaways, Takeaway } from './Takeaways';
